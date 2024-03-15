55aecf5c842b05931a59d3305d323d0faacad4ea
Murilo <murilo345666@gmail.com>
2024-03-15 09:51:45 -0300

    Adicionando título ao gráfico

--- geragrafico.py
+++ geragrafico.py
@@ -6,8 +6,10 @@ if __name__ == '__main__':
     leitor = LeitorArquivo("data.txt")
     listaValores = leitor.getValores()
     print(listaValores)     
-
+    
+    plt.title('Gráfico de linhas')
     plt.ylabel('Valores de entrada')
     plt.xlabel('Amostragem')
+    
     plt.plot(listaValores)
     plt.show()
