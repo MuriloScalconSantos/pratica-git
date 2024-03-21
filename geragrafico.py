4dfb08bd522a9e7003846eb26624c68c4963ea70
Murilo <murilo345666@gmail.com>
2024-03-21 09:49:00 -0300

    Inserindo labels para os eixos horizontal e vertical do gráfico de médias

--- geragrafico.py
+++ geragrafico.py
@@ -27,6 +27,9 @@ def main():
     plt.bar(xvalues, medias)  
     plt.xticks(xvalues, ['Série ' + str(x) for x in xvalues])
     plt.title('Médias das séries')
-   
+    plt.ylabel('Valores de entrada')
+    plt.xlabel('Amostragem')
+
     plt.show()
+
 main()
